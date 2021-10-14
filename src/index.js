b85b3615d5c99b1ad2a7ed057f6bc57503ca8a9d
riko <79884989+RIKO-tlov@users.noreply.github.com>
2021-10-14 11:03:02 +0900

    エクスクラメーションマーク

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
-var val1 = "var変数";
-console.log(val1); // var変数
+// var val1 = "var変数";
+// console.log(val1); // var変数
 
-// var変数は上書き可能
-val1 = "var変数を上書き";
-console.log(val1);
+// // var変数は上書き可能
+// val1 = "var変数を上書き";
+// console.log(val1);
 
-// var変数は再宣言可能
-var val1 = "var変数を再宣言";
-console.log(val1); // var変数を再宣言
+// // var変数は再宣言可能
+// var val1 = "var変数を再宣言";
+// console.log(val1); // var変数を再宣言
+
+//エクスクラメーションマーク 1つの場合
+// if(!true) {
+//   alert("trueです");
+// } else {
+//   alert("falseです");
+// }
+
+var a;
+if (!!a) {
+  alert("私は" + a + "です");
+} else {
+  alert("定義されていません");
+}
+
+//エクスクラメーションマーク 2つの場合
+// if(!!true) {
+//     alert("trueです");
+//   } else {
+//     alert("falseです");
+//   }
