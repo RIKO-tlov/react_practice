9e88c95c81a3e8f1f51a524d34c19e3c9a790901
riko <79884989+RIKO-tlov@users.noreply.github.com>
2021-10-16 23:57:58 +0900

    let

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // var val1 = "var変数";
-// console.log(val1); // var変数
+// console.log(val1);
 
 // // var変数は上書き可能
 // val1 = "var変数を上書き";
@@ -7,7 +7,7 @@
 
 // // var変数は再宣言可能
 // var val1 = "var変数を再宣言";
-// console.log(val1); // var変数を再宣言
+// console.log(val1);
 
 //エクスクラメーションマーク 1つの場合
 // if(!true) {
@@ -16,12 +16,12 @@
 //   alert("falseです");
 // }
 
-var a;
-if (!!a) {
-  alert("私は" + a + "です");
-} else {
-  alert("定義されていません");
-}
+// var a;
+// if (a) {
+//   alert("私は" + a + "です");
+// } else {
+//   alert("定義されていません");
+// }
 
 //エクスクラメーションマーク 2つの場合
 // if(!!true) {
@@ -29,3 +29,11 @@ if (!!a) {
 //   } else {
 //     alert("falseです");
 //   }
+
+let val2 = "let変数";
+console.log(val2);
+
+val2 = "let変数を上書き";
+console.log(val2);
+
+let val2 = "let変数を再宣言";
