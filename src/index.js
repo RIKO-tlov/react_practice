fbfe6f87a5b4cb6fc6d82912ea3d2abcf17c5423
riko <79884989+RIKO-tlov@users.noreply.github.com>
2021-10-17 00:05:16 +0900

    const

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-// var val1 = "var変数";
-// console.log(val1);
+var val1 = "var変数";
+console.log(val1);
 
-// // var変数は上書き可能
-// val1 = "var変数を上書き";
-// console.log(val1);
+// var変数は上書き可能
+val1 = "var変数を上書き";
+console.log(val1);
 
-// // var変数は再宣言可能
-// var val1 = "var変数を再宣言";
-// console.log(val1);
+// var変数は再宣言可能
+var val1 = "var変数を再宣言";
+console.log(val1);
 
-//エクスクラメーションマーク 1つの場合
+// エクスクラメーションマーク 1つの場合
 // if(!true) {
 //   alert("trueです");
 // } else {
@@ -23,7 +23,7 @@
 //   alert("定義されていません");
 // }
 
-//エクスクラメーションマーク 2つの場合
+// エクスクラメーションマーク 2つの場合
 // if(!!true) {
 //     alert("trueです");
 //   } else {
@@ -36,4 +36,11 @@ console.log(val2);
 val2 = "let変数を上書き";
 console.log(val2);
 
-let val2 = "let変数を再宣言";
+// let val2 = "let変数を再宣言";
+
+const val3 = "const変数";
+console.log(val3);
+
+// val3 = "const変数の上書き";
+
+// const val3 = "const変数の再宣言";
